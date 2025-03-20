a6035bfa56ae588dbf3463ad88eae70aba862d6b
confidential <macprom2@Macs-MacBook-Pro.local>
2025-03-20 20:57:59 +0300

    4th project

---
+++ number guess game.py
@@ -0,0 +1,29 @@
+import random
+
+highest_num = 100
+lowest_num = 1
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+whilerunning = True
+
+print("------Python guessing game------")
+print(f"----guess a number between {lowest_num}  {highest_num}----")
+
+while whilerunning:
+    guess = input("guess a number:")
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        
+        if guess < lowest_num or guess > highest_num:
+            print(" number out of range")
+            print(f"----guess a number between {lowest_num}  {highest_num}----")
+        elif guess < answer:
+            print("too low")
+        elif guess > answer:
+            print("too high")
+        else:
+            print("correct")
+            whilerunning = False
+print(f"he correct answer is: {answer}")
+print(f"you have {guesses} times")
